23922d6041f62f7f727fd46bd218abf804c32fc9
marinacor1 <marinaspam1234@gmail.com>
2016-02-22 12:27:40 -0700

    adding new files

---
+++ test/distributor_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/http/distributor'
+
+class DistributorTest < Minitest::Test
+def test_response_says_hello_world_with_number
+  s = Server.new
+  s.response
+  assert_equal "Hello World (0)", headers
+end
+
+end
---
+++ test/response_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/http/response'
+
+class ResponseTest < Minitest::Test
+def test_response_says_hello_world_with_number
+  skip 
+  r = Response.new
+  s.hello_world
+  assert_equal "Hello World (0)", headers
+end
+
+end
