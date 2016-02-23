bb81e595031c34b21f35433f3718ddee71d8283c
marinacor1 <marinaspam1234@gmail.com>
2016-02-22 20:42:05 -0700

    adding tests to d test

--- test/distributor_test.rb
+++ test/distributor_test.rb
@@ -3,10 +3,30 @@ require 'minitest/pride'
 require_relative '../lib/http/distributor'
 
 class DistributorTest < Minitest::Test
-def test_response_says_hello_world_with_number
-  s = Server.new
-  s.response
-  assert_equal "Hello World (0)", headers
-end
+  def test_distributor_instantiates
+    distributor = Distributor.new
+    distributor.instance_of? Distributor
+  end
+
+  def test_response_says_hello_world_with_number
+    skip
+    server = Distributor.new
+    server.response
+    assert_equal "Hello World (0)", response(0)
+  end
+
+  def test_response_says_hello_world_with_different_number
+    skip
+    server = Distributor.new
+    server.response
+    assert_equal "Hello World (25)", response(25)
+  end
+
+  def test_output_passes_response_to_response_method
+    skip
+    server = Distributor.new
+    assert_equal "Hello World (0)", server.output(response(5))
+  end
+
 
 end
