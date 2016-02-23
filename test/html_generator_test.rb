afc646bb9d70d38c02b71e97a425ee26f7599cce
marinacor1 <marinaspam1234@gmail.com>
2016-02-23 16:41:45 -0700

    adding other test for html test ;

--- test/html_generator_test.rb
+++ test/html_generator_test.rb
@@ -4,9 +4,12 @@ require_relative '../lib/http/html_generator'
 require 'pry'
 
 class HTMLGeneratorTest < Minitest::Test
-  def test_wrap_respons_adds_pre_before_and_after_content
+  def test_wrap_response_adds_pre_before_and_after_content
     assert_equal "<pre> Hi there </pre>", wrap_response("Hi there ")
+  end
 
+  def test_generate_html_adds_html_content_to_wrapped_response
+    assert_equal "<pre> Hi there </pre>", generate_html("Hi there ")
   end
 
 end
