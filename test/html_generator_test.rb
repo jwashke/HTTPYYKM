9c97e52b73e6ef0a8dbe387b50de9b8c794778a2
Josh Washke <jwashke@icloud.com>
2016-02-23 17:19:15 -0700

    Made html generator tests pass

--- test/html_generator_test.rb
+++ test/html_generator_test.rb
@@ -1,17 +1,20 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/http/html_generator'
+require_relative 'test_helper'
 require 'pry'
 
 class HTMLGeneratorTest < Minitest::Test
+  def setup
+    @test_helper = TestHelper.new
+  end
+
   def test_wrap_response_adds_pre_before_and_after_content
-    skip
-    assert_equal "<pre> Hi there </pre>", wrap_response("Hi there ")
+    assert_equal "<pre>Hi there</pre>", @test_helper.wrap_response("Hi there")
   end
 
   def test_generate_html_adds_html_content_to_wrapped_response
-    skip 
-    assert_equal "<pre> Hi there </pre>", generate_html("Hi there ")
+    assert_equal "<html><head></head><body><pre>Hi there</pre></body></html>", @test_helper.generate_html("Hi there")
   end
 
 end
