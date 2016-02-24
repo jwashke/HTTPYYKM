a7a3ea0ca05721d9c98066f8c5fb69ac3cbe8b2e
marinacor1 <marinaspam1234@gmail.com>
2016-02-24 10:06:19 -0700

    adding initializing tests

--- test/game_test.rb
+++ test/game_test.rb
@@ -4,9 +4,31 @@ require_relative '../lib/http/game'
 
 class GameTest < Minitest::Test
   def test_game_starts_with_good_luck_message
+    skip
+    #can move to response_generator_test just keeping it here for now
+    rg = ResponseGenerator.new
+    assert_equal "Good Luck!", rg.game_start
+  end
+
+  def test_game_instantiates
+    skip
+    game = Game.new
+    game.instance_of? Game
+  end
+
+  def test_game_instantiates_guess_count_as_zero
+    skip
+    game = Game.new
+    assert_equal 0, game.guess_count
   end
 
   def test_game_generates_random_number
+    skip
+    game = Game.new
+    correct_num_one  = game.random_num_generator
+    game = Game.new 
+    correct_num_two  = game.random_num_generator
+    refute correct_num_one == correct_num_two
   end
 
   def test_game_tells_too_high
