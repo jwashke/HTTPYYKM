535af8bb81dafcd50b77fb36a6dc7d70de4ecf48
marinacor1 <marinaspam1234@gmail.com>
2016-02-24 11:11:30 -0700

    adding all game stuff to rg

--- lib/http/game.rb
+++ lib/http/game.rb
@@ -1,27 +1,3 @@
-#in Response Generator
-  attr_accessor :guess_count
-  attr_reader :correct_number
-
-  def start_game
-    @game_counter = 0
-    @last_guess = nil
-    @correct_number = rand(1..100)
-    "Good Luck!"
-  end
-
-  def guessing_game(@game_counter, player_guess)
-    #rewrite player_guess if not correct. if complicted create new method
-    if @game_counter.nil?
-      "You need to start a new game first"
-    elsif @last_guess.nil?
-      @game_counter +=1
-      game = Game.new(player_guess, @game_counter, @last_guess = nil)
-    else
-      @game_counter +=1 
-      game.guess_check(player_guess, correct_number)
-    end
-  end
-
 module Game
 
   def guess_check(player_guess, correct_number)
@@ -34,10 +10,5 @@ module Game
     end
 
   end
-
-
-
-
-
 
 end
--- lib/http/response_generator.rb
+++ lib/http/response_generator.rb
@@ -4,6 +4,8 @@ require_relative 'game'
 module HTTP
   class ResponseGenerator
     include StatusCodes
+    attr_accessor :guess_count
+    attr_reader :correct_number
 
     def initialize
       @hello_count    = 0
@@ -34,8 +36,25 @@ module HTTP
 
     end
 
-    def game
-
+    def start_game
+      @game_counter = 0
+      @last_guess = nil
+      @correct_number = rand(1..100)
+      "Good Luck!"
     end
+
+    def guessing_game(@game_counter, player_guess)
+      #rewrite player_guess if not correct. if complicted create new method
+      if @game_counter.nil?
+        "You need to start a new game first"
+      elsif @last_guess.nil?
+        @game_counter +=1
+        game = Game.new(player_guess, @game_counter, @last_guess = nil)
+      else
+        @game_counter +=1
+        game.guess_check(player_guess, correct_number)
+      end
+    end
+
   end
 end
