8a9e2d3231abc90e270d21736a91270883aa3d6c
marinacor1 <marinaspam1234@gmail.com>
2016-02-25 21:46:59 -0700

    fixing distributor tests after bug fix

--- lib/http/server.rb
+++ lib/http/server.rb
@@ -1,9 +1,5 @@
 lib_folder = File.expand_path(__dir__)
 $LOAD_PATH << lib_folder
-#if exception occurs in thread, crash exception but not thread
-#Faraday sends request. server makes new request
-#raise an exception
-#if thread crashes all program crashes
 Thread.abort_on_exception = true
 require 'pry'
 require 'socket'
--- test/distributor_test.rb
+++ test/distributor_test.rb
@@ -24,95 +24,95 @@ class DistributorTest < Minitest::Test
 
   def test_response_says_hello_world_with_get_path_hello
     path = HTTP::Path.new
-    request_hash = {:verb=>"GET", :path=>"/hello", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/hello", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     assert_equal "Hello World! (1)",  @distributor.path_checker(request_hash)
   end
 
   def test_response_says_hello_world_with_different_number
-    request_hash = {:verb=>"GET", :path=>"/hello", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/hello", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     @distributor.path_checker(request_hash)
     @distributor.path_checker(request_hash)
     assert_equal "Hello World! (3)", @distributor.path_checker(request_hash)
   end
 
   def test_when_redirect_request_called_total_requests_increases
-    request_hash = {:verb=>"GET", :path=>"/hello", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/hello", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     @distributor.redirect_request(request_hash)
     assert_equal 1, @distributor.total_requests
   end
 
   def test_path_checker_sends_to_get_path_root
-    request_hash = {:verb=>"GET", :path=>"/", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     assert @distributor.path_checker(request_hash) == @distributor.path.get_path_root(request_hash)
   end
 
   def test_path_checker_will_not_send_to_hello_if_different_path_set
-    request_hash = {:verb=>"GET", :path=>"/datetime", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/datetime", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     refute "Hello World! (3)" == @distributor.path_checker(request_hash)
   end
 
   def test_path_checker_sends_to_datetime
-    request_hash = {:verb=>"GET", :path=>"/datetime", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/datetime", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     time = Time.new.strftime("%a, %e %b %Y %H:%M:%S %z")
     assert_equal time, @distributor.path_checker(request_hash)
   end
 
   def test_path_checker_sends_to_shutdown
-    request_hash = {:verb=>"GET", :path=>"/shutdown", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/shutdown", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     assert_equal "Total Requests: 0", @distributor.path_checker(request_hash)
   end
 
   def test_path_checker_sends_to_start_game
-    request_hash = {:verb=>"GET", :path=>"/start_game", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/start_game", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     assert_equal "Good luck!", @distributor.path_checker(request_hash)
   end
 
   def test_path_checker_sends_to_game_with_notice_if_start_game_hasnt_happened
-    request_hash = {:verb=>"GET", :path=>"/start_game", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/start_game", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     @distributor.path_checker(request_hash)
-    request_hash = {:verb=>"GET", :path=>"/game", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/game", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     assert_equal "Please make a guess between 1 and 100", @distributor.path_checker(request_hash)
   end
 
   def test_path_checker_sends_to_word_search_if_word_all_caps
-    request_hash = {:verb=>"GET", :path=>"/word_search", :word=>"COFFEE", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/word_search", :word=>"COFFEE", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     assert_equal "Coffee is a known word.", @distributor.path_checker(request_hash)
   end
 
   def test_path_checker_sends_to_word_search
-    request_hash = {:verb=>"GET", :path=>"/word_search", :word=>"coffee", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/word_search", :word=>"coffee", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     assert_equal "Coffee is a known word.", @distributor.path_checker(request_hash)
   end
 
   def test_path_checker_sends_to_word_search_with_incorrect_word
-    request_hash = {:verb=>"GET", :path=>"/word_search", :word=>"cfryye", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/word_search", :word=>"cfryye", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     assert_equal "Cfryye is not a known word.", @distributor.path_checker(request_hash)
   end
 
   def test_path_checker_sends_to_error_path
-    request_hash = {:verb=>"GET", :path=>"/force_error", :word=>"cfryye", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/force_error", :word=>"cfryye", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     back_trace = @distributor.path_checker(request_hash)
     assert back_trace.include?("Users")
   end
 
   def test_path_checker_sends_to_404_if_incorrect_path
-    request_hash = {:verb=>"GET", :path=>"/lolz", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/lolz", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     assert_equal "404 Not Found", @distributor.path_checker(request_hash)
   end
 
   def test_path_checker_sends_to_404_if_incorrect_path_different
-    request_hash = {:verb=>"GET", :path=>"/wowww", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/wowww", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     assert_equal "404 Not Found", @distributor.path_checker(request_hash)
   end
 
   def test_shutdown_returns_correct_total_requests_for_one
-    request_hash = {:verb=>"GET", :path=>"/shutdown", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/shutdown", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     @distributor.redirect_request(request_hash)
     assert_equal "Total Requests: 1", @distributor.path_checker(request_hash)
   end
 
   def test_shutdown_returns_correct_total_requests_for_multiples
-    request_hash = {:verb=>"GET", :path=>"/shutdown", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
+    request_hash = {:verb=>"GET", 'Path'=>"/shutdown", :protocol=>"HTTP/1.1", :host=>" 127.0.0.1", :port=>"9292", :origin=>" 127.0.0.1", :Accept=>"*/*"}
     @distributor.redirect_request(request_hash)
     @distributor.redirect_request(request_hash)
     @distributor.redirect_request(request_hash)
