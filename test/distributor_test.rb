2043dbb02cb7cfc5cd8dcb30f063c6ea6f6ebf0a
marinacor1 <marinaspam1234@gmail.com>
2016-02-25 21:50:27 -0700

    fixing hash issue

--- test/distributor_test.rb
+++ test/distributor_test.rb
@@ -129,7 +129,7 @@ class DistributorTest < Minitest::Test
   def test_generate_output_header_has_correct_beginning
     output = "Total Requests: 1"
     @distributor.generate_output(output)
-    assert_equal "http/1.1", @distributor.header.split[0]
+    assert_equal "HTTP/1.1", @distributor.header.split[0]
   end
 
   def test_generate_output_gives_correct_header_for_a_diff_status_code
